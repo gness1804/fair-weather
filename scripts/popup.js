f05de3e838cbba7dbcc1c219f197f37205b8e88f
gness1804 <graham.nessler@gmail.com>
2017-05-10 16:56:29 -0500

    Zip code value now displays on page

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
+const zip = document.querySelector('#zip-entry-field')
+const submitButton = document.querySelector('#submit-button')
+submitButton.addEventListener('click', () => {
+  const output = document.querySelector('#output')
+  output.innerText = zip.value
 })
+
