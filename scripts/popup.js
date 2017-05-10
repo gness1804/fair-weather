8a47b216b323d47f7393019b32c13d0c3fd906c5
gness1804 <graham.nessler@gmail.com>
2017-05-10 17:07:30 -0500

    Create new API call function

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,15 @@
 const zip = document.querySelector('#zip-entry-field')
 const submitButton = document.querySelector('#submit-button')
+
+const makeMainAPICall = () => {
+//   const output = document.querySelector('#output')
+//   output.innerText = zip.value
+  const zipVal = zip.value
+}
+
 submitButton.addEventListener('click', () => {
-  const output = document.querySelector('#output')
-  output.innerText = zip.value
+  makeMainAPICall()
 })
+
+
 
