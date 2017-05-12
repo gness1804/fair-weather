9a5479d7ae5290560f0c93d3234b01a73a9aecb2
gness1804 <graham.nessler@gmail.com>
2017-05-11 20:33:56 -0500

    Makes API call to get weather from zip

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,9 +2,23 @@ const zip = document.querySelector('#zip-entry-field')
 const submitButton = document.querySelector('#submit-button')
 
 const makeMainAPICall = () => {
-//   const output = document.querySelector('#output')
-//   output.innerText = zip.value
   const zipVal = zip.value
+  const hitAPI = new XMLHttpRequest();
+  const url = `http://api.wunderground.com/api/47fe8304fc0c9639/conditions/q/${zipVal}.json`;
+  hitAPI.open('GET', url, true);
+  hitAPI.send();
+  hitAPI.onreadystatechange = () => {
+    if (hitAPI.readyState === XMLHttpRequest.DONE) {
+      if (hitAPI.status === 200) {
+        const temp = JSON.parse(hitAPI.responseText).current_observation.temp_f
+        // temporary for debugging 
+        const output = document.querySelector('#output')
+        output.innerText = temp
+      } else {
+        alert('There was a problem with fetching your data. Please ensure that you have entered a five-digit US Zip code and try again.')
+      }
+    }
+  }
 }
 
 submitButton.addEventListener('click', () => {
