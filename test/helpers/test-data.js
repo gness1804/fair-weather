fe635d7ffdffb4667361a7a2df60953f7289bc92
gness1804 <graham.nessler@gmail.com>
2017-06-04 15:35:20 -0500

    Add passing tests for invalid zip

--- test/helpers/test-data.js
+++ test/helpers/test-data.js
@@ -1,7 +1,11 @@
 const validZip = '48109';
+const invalidZipText = 'foo';
+const invalidZipLength = '2406';
 
 if (typeof document === 'undefined') {
   module.exports = {
     validZip,
+    invalidZipText,
+    invalidZipLength,
   };
 }
--- test/validateZip-test.js
+++ test/validateZip-test.js
@@ -1,7 +1,7 @@
 /* global it, expect, describe, assert */
 const assert = require('chai').assert;
 const validateZip = require('../helpers/validateZip');
-const { validZip } = require('./helpers/test-data');
+const { validZip, invalidZipText, invalidZipLength } = require('./helpers/test-data');
 
 describe('validateZip', () => {
   it('should be a function', () => {
@@ -10,5 +10,11 @@ describe('validateZip', () => {
 
   it('should return true for a valid zip code', () => {
     assert.strictEqual(validateZip(validZip), true);
+  });
+
+  it('should return false for an invalid zip code', () => {
+    assert.strictEqual(validateZip(''), false);
+    assert.strictEqual(validateZip(invalidZipText), false);
+    assert.strictEqual(validateZip(invalidZipLength), false);
   });
 });
