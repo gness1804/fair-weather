5043f261f0c36d3d14083b83f683eaa1614192be
gness1804 <graham.nessler@gmail.com>
2017-06-04 15:55:04 -0500

    Add passing test for populateTemp

--- test/helpers/test-data.js
+++ test/helpers/test-data.js
@@ -1,11 +1,17 @@
 const validZip = '48109';
 const invalidZipText = 'foo';
 const invalidZipLength = '2406';
+const fakeTemp = 60;
+const fakeCity = 'Blacksburg';
+const fakeState = 'VA';
 
 if (typeof document === 'undefined') {
   module.exports = {
     validZip,
     invalidZipText,
     invalidZipLength,
+    fakeTemp,
+    fakeCity,
+    fakeState,
   };
 }
--- test/populateTemp-test.js
+++ test/populateTemp-test.js
@@ -1,6 +1,7 @@
 /* global it, expect, describe, assert */
 const assert = require('chai').assert;
 const { populateTemp, output } = require('../helpers/populateTemp');
+const { fakeTemp, fakeCity, fakeState } = require('./helpers/test-data');
 
 describe('populateTemp', () => {
   it('should be a function', () => {
@@ -9,5 +10,10 @@ describe('populateTemp', () => {
 
   it('should contain an output object', () => {
     assert.isDefined(output);
+  });
+
+  it('should return expected data with a given input', () => {
+    populateTemp(fakeTemp, fakeCity, fakeState);
+    assert.strictEqual(output.innerText, 'The temperature in Blacksburg, VA is 60 degrees F.');
   });
 });
