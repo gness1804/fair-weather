7cf08baaefa821bbb91b9ca238449a4fc8fa2475
gness1804 <graham.nessler@gmail.com>
2017-06-04 15:01:27 -0500

    Set up testing suite

---
+++ test/validateZip-test.js
@@ -0,0 +1,8 @@
+/* global it, expect, describe, assert */
+const assert = require('chai').assert;
+
+describe('testing suite works', () => {
+  it('works', () => {
+    assert.equal(1, 1);
+  });
+});
